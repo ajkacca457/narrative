80f028de14928df2455211e7368e929410063713
Avijit Karmaker <ajkacca@gmail.com>
2024-04-18 09:20:03 +0300

    update navbar for the site

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import MainLogo from '../assets/images/main-logo.png'
+import { navbardata } from '../data/sitedata'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='grid grid-cols-6 place-items-center'>
+        
+        <img src={MainLogo} alt='Main Logo' className='col-span-1'/>
+        
+        <div className='col-span-4'>
+            {navbardata.map((item, index) => {
+                return(
+                    <NavLink key={index} to={item.path} activeClassName='text-red-500'>{item.name}</NavLink>
+                )
+            })}
+        </div>
+
+        <div className='col-span-1'>
+            <button >Book a Call</button>
+        </div>
+
+    </div>
   )
 }
 
---
+++ src/data/sitedata.js
@@ -0,0 +1,23 @@
+export const navbardata = [
+
+    {
+        path: "/platform",
+        name: "Platform",
+    },
+    {
+        path: "/what-we-do",
+        name: "What We Do",
+    },
+    {
+        path:"/services",
+        name: "Services",
+    },
+    {
+        path:"/how-it-works",
+        name: "How It Works",
+    },
+    {
+        path:"/faq",
+        name: "FAQ",
+    }
+];
