4ca896ed19398abe319d316faca5cebbfd681bd4
Avijit Karmaker <ajkacca@gmail.com>
2024-04-18 09:25:47 +0300

    update bg-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'primary': '#fcfcf4'
+      },
+    },
   },
   plugins: [],
 }
