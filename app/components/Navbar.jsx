4a432c4a81299ddbee03d85a3c9ad8f81f56587b
Avijit Karmaker <ajkacca@gmail.com>
2024-10-20 14:16:02 +0300

    update navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
 
 const Navbar = () => {
-  return (
-    <div className='max-w-[90rem] mx-auto w-[90%] flex justify-between items-center py-4'>
-        <h2>Narrative</h2>
-        <Image src='/arrow.svg' alt='logo' width={30} height={30} />
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  return (
+    <div className="max-w-[90rem] mx-auto w-[90%] flex justify-between items-center py-4">
+      <h2>Narrative</h2>
+      <Image
+        src="/arrow.svg"
+        alt="logo"
+        width={30}
+        height={30}
+        className={`z-[1002] transition-transform duration-300 ${
+          isOpen ? "-rotate-45" : "rotate-0"
+        }`}
+        onClick={() => {
+          setIsOpen(!isOpen);
+        }}
+      />
+
+      <div
+        className={`fixed bg-[#F3FDB8] h-[100vh] w-[50vw] top-0 right-0 z-[1001] transition-all duration-300 ${
+          isOpen ? "translate-x-0" : "translate-x-[50vw]"
+        } `}
+      >
+        <ul className="z-50">
+          <li className="text-white">Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
