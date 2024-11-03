f4460021182e38cb83a81cab9a983403769d39f2
Avijit Karmaker <ajkacca@gmail.com>
2024-11-03 14:06:33 +0200

    fix showreel position

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div className="max-w-[90rem] w-[90%] mx-auto">
-        <iframe className='w-full aspect-video' src="https://www.youtube.com/embed/mZ5hnNRBFsc?si=kxF37tb8Nm-BoeNI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-    </div>
-  )
-}
+    <div className="max-w-[90rem] w-[90%] mx-auto mt-[5vh]">
+<div className="relative w-full pt-[56.25%]">
+  <iframe 
+    className="w-full h-full absolute top-0 left-0" 
+    src="https://www.youtube.com/embed/mZ5hnNRBFsc?si=kxF37tb8Nm-BoeNI&autoplay=1&loop=1&controls=0&playlist=mZ5hnNRBFsc&mute=1" 
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+    referrerPolicy="strict-origin-when-cross-origin">
+  </iframe>
+  <div className="absolute top-0 left-0 w-full h-full"></div> 
+</div>
 
-export default Hero
+    </div>
+  );
+};
+
+export default Hero;
