27fb5288736cf129843a694cdc0db77395c1edad
Avijit Karmaker <ajkacca@gmail.com>
2024-11-03 15:18:59 +0200

    update video

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,13 +2,25 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <div className="max-w-[90rem] w-[90%] mx-auto py-[10vh] about">
-        <div className="grid grid-cols-4">
+    <div className="relative">
+    <video
+          autoPlay
+          muted
+          loop
+          playsInline
+          className="w-full h-full object-cover absolute top-0 left-0 aspect-video"
+        >
+          <source src="/communication.mp4" type="video/mp4" />
+        </video>
+        <div className="absolute top-0 left-0 w-full h-full bg-[#D1CCE6] opacity-90"></div>
+      <div className="max-w-[90rem] w-[90%] min-h-[100vh] mx-auto about relative">
+        <div className="grid grid-cols-4 absolute w-full h-full top-0 left-0 z-30 py-[10vh]">
           <h1 className="text-8xl col-span-3">
-            We fuel organic growth enhancing your brand’s <span className="font-bold">identity</span>, driving
-            real <span className="font-bold">engagement</span>, and generating <span className="font-bold">leads</span> across all <span className="font-bold">social media
-            platforms</span>.
+            We fuel organic growth enhancing your brand’s{" "}
+            <span className="font-bold">identity</span>, driving real{" "}
+            <span className="font-bold">engagement</span>, and generating{" "}
+            <span className="font-bold">leads</span> across all{" "}
+            <span className="font-bold">social media platforms</span>.
           </h1>
         </div>
       </div>
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="max-w-[90rem] w-[90%] mx-auto mt-[5vh]">
-<div className="relative w-full pt-[56.25%]">
+    <div className="max-w-[90rem] w-[90%] mx-auto mt-[5vh] relative">
+<div className="w-full pt-[56.25%]">
   <iframe 
     className="w-full h-full absolute top-0 left-0" 
     src="https://www.youtube.com/embed/mZ5hnNRBFsc?si=kxF37tb8Nm-BoeNI&autoplay=1&loop=1&controls=0&playlist=mZ5hnNRBFsc&mute=1" 
