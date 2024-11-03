74e00b9f5c900ca78f937771540fa2556b1be531
Avijit Karmaker <ajkacca@gmail.com>
2024-11-03 23:52:12 +0200

    add border-radius to hero

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="max-w-[90rem] w-[90%] mx-auto my-[5vh] relative">
       <div className="w-full pt-[56.25%]">
         <iframe
-          className="w-full h-full absolute top-0 left-0"
+          className="w-full h-full absolute top-0 left-0 rounded-xl"
           src="https://www.youtube.com/embed/mZ5hnNRBFsc?si=kxF37tb8Nm-BoeNI&autoplay=1&loop=1&controls=0&playlist=mZ5hnNRBFsc&mute=1"
           title="YouTube video player"
           frameBorder="0"
--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -8,7 +8,7 @@ const Projects = () => {
     <div>
       <div className="max-w-[90rem] w-[90%] mx-auto py-[10vh]">
         <h1 className="text-6xl text-white mb-10">Projects</h1>
-        <Masonry>
+        <Masonry className="gap-x-4">
           <img src="./project1.jpg" alt="placeholder" className="m-2" />
           <img src="./project2.jpg" alt="placeholder" className="m-2"/>
           <img src="./project1.jpg" alt="placeholder" className="m-2"/>
