1fc522978522f33c1b2c0476d8714c1a8c432135
Avijit Karmaker <ajkacca@gmail.com>
2025-01-15 00:17:35 +0200

    add input element

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,65 +4,62 @@ const Footer = () => {
   return (
     <div>
       <div className="max-w-[90rem] w-[90%] mx-auto py-[10vh] text-white">
-        <div className="grid grid-cols-4 gap-10">
-          <div className="col-span-1">
-            <h2 className="text-xl">Join our newsletter</h2>
-            <p className="mt-4">
-              Leave us with your email to get updates from the studio
-            </p>
-            <input
-              type="text"
-              name=""
-              id=""
-              className="w-full px-2 mt-2"
-              placeholder="name@example.com"
-            />
+        <div className="grid grid-cols-5 gap-[5vw]">
+          <div className="col-span-2">
+            <div className="text-[17px] text-[#7F7F7F]">
+              <p>Made using clean, non-toxic ingredients, our products are </p>
+              <p>signed for everyone. Made using clean, non-toxic</p>
+              <p>ingredients, our products are signed</p>
+            </div>
+
+            <div className="mt-8 flex items-center gap-x-4">
+              <img src="/footerimg/phone-icon.svg" alt="logo" />
+              <h4>Street Address, Phone, Other info</h4>
+            </div>
+
+            <div>
+              <h2 className="text-[22px] mt-[10vh]">Newsletter</h2>
+              <div className="flex">
+                <input
+                  type="text"
+                  name=""
+                  id=""
+                  className="w-full px-2 py-4 mt-2"
+                  placeholder="name@example.com"
+                />
+                <button className="bg-[#00FFE1] text-black w-1/2 py-4 mt-2">
+                  Subscribe
+                </button>
+              </div>
+            </div>
           </div>
 
-          <div className="col-span-1 col-start-3">
-            <h2 className="text-xl">Visit Us</h2>
-            <p className="mt-4">
-              1st Floor, 96 Church Street, Brighton, BN1 1UJ Contact Us +44(0)
-              1273 921 215 ‍studio@buffmotion.com
-            </p>
-          </div>
+          <div className="col-span-3">
+            <div>
+              <h2 className="text-xl">Visit Us</h2>
+              <p className="mt-4">
+                1st Floor, 96 Church Street, Brighton, BN1 1UJ Contact Us +44(0)
+                1273 921 215 ‍studio@buffmotion.com
+              </p>
+            </div>
 
-          <div className="col-span-1 col-start-4">
-            <h2 className="text-xl">Contact Us</h2>
-            <div className="paragraph-0-9">
-              <a href="tel:+441273921215" className="u-text-black">
-                +44(0) 1273 921 215
-              </a>{" "}
-              <br />‍
-              <a href="mailto:‍studio@buffmotion.com" className="u-text-black">
-                studio@buffmotion.com
-              </a>
+            <div>
+              <h2 className="text-xl">Contact Us</h2>
+              <div className="paragraph-0-9">
+                <a href="tel:+441273921215" className="u-text-black">
+                  +44(0) 1273 921 215
+                </a>{" "}
+                <br />‍
+                <a
+                  href="mailto:‍studio@buffmotion.com"
+                  className="u-text-black"
+                >
+                  studio@buffmotion.com
+                </a>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="grid grid-cols-4 gap-10 mt-[10vh]">
-          <div className="col-span-1 grid grid-cols-4 gap-6">
-            <img src="/dummy-logo.svg" alt="logo" className="col-span-1" />
-            <img src="/dummy-logo.svg" alt="logo" className="col-span-1" />
-            <img src="/dummy-logo.svg" alt="logo" className="col-span-1" />
-            <img src="/dummy-logo.svg" alt="logo" className="col-span-1" />
-          </div>
-
-          <div className="col-span-1 col-start-4">
-            <h2 className="text-xl">Contact Us</h2>
-            <div className="paragraph-0-9">
-              <a href="tel:+441273921215" className="u-text-black">
-                +44(0) 1273 921 215
-              </a>{" "}
-              <br />‍
-              <a href="mailto:‍studio@buffmotion.com" className="u-text-black">
-                studio@buffmotion.com
-              </a>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
   );
