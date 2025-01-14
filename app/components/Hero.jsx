7e995890ec863b457c9260cb5bed0b5279da6d5d
Avijit Karmaker <ajkacca@gmail.com>
2025-01-14 08:42:29 +0200

    add hero element

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -2,17 +2,14 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="max-w-[90rem] w-[90%] mx-auto my-[5vh] relative">
-      <div className="w-full pt-[56.25%]">
-        <iframe
-          className="w-full h-full absolute top-0 left-0 rounded-xl"
-          src="https://www.youtube.com/embed/mZ5hnNRBFsc?si=kxF37tb8Nm-BoeNI&autoplay=1&loop=1&controls=0&playlist=mZ5hnNRBFsc&mute=1"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-        ></iframe>
-        <div className="absolute top-0 left-0 w-full h-full"></div>
+    <div className="w-full h-screen relative flex flex-col items-center justify-center">
+      <div className=" max-w-[1100px]">
+        <h1 className="text-[96px] text-white font-semibold">
+          We are a digital marketing agency with expertise
+        </h1>
+        <h5 className="text-[20px] ml-[100px] tracking-[.3em] text-white mt-6 uppercase">
+          a brand design studio.
+        </h5>
       </div>
     </div>
   );
