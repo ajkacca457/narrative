0513a6758c46602b6d41bd14036ee8bc3ee1eb5a
Avijit Karmaker <ajkacca@gmail.com>
2025-01-17 21:33:53 +0200

    add grey boxes in footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -35,29 +35,126 @@ const Footer = () => {
           </div>
 
           <div className="col-span-3">
-
             <div className="grid grid-cols-3 gap-x-4">
               <div className="col-span-1">
-                  <div className="flex items-center gap-x-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>
-                  <div className="flex items-center gap-x-2 mt-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>
-                  <div className="flex items-center gap-x-2 mt-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>
-                  <div className="flex items-center gap-x-2 mt-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>                  
+                <div className="flex items-center gap-x-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
               </div>
 
               <div className="col-span-1">
-                <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded">Facebook</button>  
-                <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded">Instagram</button>
+                <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded">
+                  Facebook
+                </button>
+                <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded">
+                  Instagram
+                </button>
               </div>
 
               <div className="col-span-1">
-                  <div className="flex items-center gap-x-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>
-                  <div className="flex items-center gap-x-2 mt-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>
-                  <div className="flex items-center gap-x-2 mt-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>
-                  <div className="flex items-center gap-x-2 mt-2"> <img src="/green-arrow.png" /> <h3>Recent Work</h3></div>                  
+                <div className="flex items-center gap-x-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
               </div>
-            
             </div>
 
+            <div className="grid grid-cols-4 gap-x-4 mt-[10vh]">
+              <div className="col-span-2 flex gap-x-2">
+                <svg
+                  width="112"
+                  height="81"
+                  viewBox="0 0 112 81"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
+                    fill="#4B4B4B"
+                  />
+                </svg>
+                <svg
+                  width="112"
+                  height="81"
+                  viewBox="0 0 112 81"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
+                    fill="#4B4B4B"
+                  />
+                </svg>
+                <svg
+                  width="112"
+                  height="81"
+                  viewBox="0 0 112 81"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
+                    fill="#4B4B4B"
+                  />
+                </svg>
+                <svg
+                  width="112"
+                  height="81"
+                  viewBox="0 0 112 81"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
+                    fill="#4B4B4B"
+                  />
+                </svg>
+              </div>
+
+              <div className="col-span-2">
+                <div className="flex items-center gap-x-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  {" "}
+                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
