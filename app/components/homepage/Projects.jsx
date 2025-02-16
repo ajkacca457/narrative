aa9a58563f68bad0a89e3164658f4cff0bc738ff
Avijit Karmaker <ajkacca@gmail.com>
2025-02-16 17:56:25 +0200

    update image sizes

--- app/components/homepage/Projects.jsx
+++ app/components/homepage/Projects.jsx
@@ -49,7 +49,7 @@ const Projects = ({content}) => {
         {project.length && project.map((data,index) => (    
 
             <div key={index} className="project-card px-4">
-                <img src={data?.image?.sizes?.medium} alt="project" className="w-full h-[300px] object-cover" />
+                <img src={data?.image?.sizes?.large} alt="project" className="w-full h-[300px] object-cover" />
                 <div className="project-card-content mt-10">
  
                     <div className="flex justify-between items-center mt-4 border-b border-[#1E1E21] pb-4">
