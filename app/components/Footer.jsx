3fe2ac0bbe42178cc977ea74c3f63f83bb64d1a3
Avijit Karmaker <ajkacca@gmail.com>
2025-02-18 16:46:55 +0200

    add new footer design

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,150 +3,45 @@ import React from "react";
 const Footer = () => {
   return (
     <div>
-      <div className="max-w-[90rem] w-[90%] mx-auto py-[10vh] text-white">
-        <div className="grid grid-cols-5 gap-[5vw]">
-          <div className="col-span-2">
-            <div className="text-[17px] text-[#7F7F7F]">
-              <p>Made using clean, non-toxic ingredients, our products are </p>
-              <p>signed for everyone. Made using clean, non-toxic</p>
-              <p>ingredients, our products are signed</p>
+      <div className="max-w-[90rem] w-[90%] mx-auto pt-[5vh] pb-[20px] text-white">
+        <div className="flex justify-between">
+          <div>
+            <img src="./narrative-logo.png" alt="logo" className="mb-2" />
+            <div className="flex items-center gap-x-2">
+              {" "}
+              <img src="/green-arrow.png" /> <h3>( 021 ) 666 888 0000</h3>
             </div>
-
-            <div className="mt-8 flex items-center gap-x-4">
-              <img src="/footerimg/phone-icon.svg" alt="logo" />
-              <h4>Street Address, Phone, Other info</h4>
+            <div className="flex items-center gap-x-2 mt-2">
+              {" "}
+              <img src="/green-arrow.png" /> <h3>needhelp@company.com</h3>
             </div>
-
-            <div>
-              <h2 className="text-[22px] mt-[10vh]">Newsletter</h2>
-              <div className="flex">
-                <input
-                  type="text"
-                  name=""
-                  id=""
-                  className="w-full px-2 py-4 mt-2"
-                  placeholder="name@example.com"
-                />
-                <button className="bg-[#00FFE1] text-black w-1/2 py-4 mt-2">
-                  Subscribe
-                </button>
-              </div>
+            <div className="flex items-center gap-x-2 mt-2">
+              <img src="/green-arrow.png" />
+              <p>Privacy Policy</p>
             </div>
           </div>
 
-          <div className="col-span-3">
-            <div className="grid grid-cols-3 gap-x-4">
-              <div className="col-span-1">
-                <div className="flex items-center gap-x-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-                <div className="flex items-center gap-x-2 mt-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-                <div className="flex items-center gap-x-2 mt-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-                <div className="flex items-center gap-x-2 mt-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-              </div>
-
-              <div className="col-span-1">
-                <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded">
-                  Facebook
-                </button>
-                <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded">
-                  Instagram
-                </button>
-              </div>
-
-              <div className="col-span-1">
-                <div className="flex items-center gap-x-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-                <div className="flex items-center gap-x-2 mt-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-                <div className="flex items-center gap-x-2 mt-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-                <div className="flex items-center gap-x-2 mt-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>Recent Work</h3>
-                </div>
-              </div>
+          <div>
+            <div className="grid grid-cols-2 gap-x-4">
+              <button className="bg-[#00FFE1] transition-colors duration-300 hover:bg-[#00CCB5] text-black w-fit block px-4 py-2 mt-2 rounded col-span-1">
+                Facebook
+              </button>
+              <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded col-span-1">
+                Instagram
+              </button>
+              <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded col-span-1">
+                Instagram
+              </button>
+              <button className="bg-[#00FFE1] text-black w-fit block px-4 py-2 mt-2 rounded col-span-1">
+                Instagram
+              </button>
             </div>
+          </div>
+        </div>
 
-            <div className="grid grid-cols-4 gap-x-4 mt-[10vh]">
-              <div className="col-span-2 flex items-end gap-x-2">
-                <svg
-                  width="112"
-                  height="81"
-                  viewBox="0 0 112 81"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
-                    fill="#4B4B4B"
-                  />
-                </svg>
-                <svg
-                  width="112"
-                  height="81"
-                  viewBox="0 0 112 81"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
-                    fill="#4B4B4B"
-                  />
-                </svg>
-                <svg
-                  width="112"
-                  height="81"
-                  viewBox="0 0 112 81"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
-                    fill="#4B4B4B"
-                  />
-                </svg>
-                <svg
-                  width="112"
-                  height="81"
-                  viewBox="0 0 112 81"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M101.53 0.0170898H9.99976C4.47691 0.0170898 -0.000244141 4.15135 -0.000244141 9.25122V71.1199C-0.000244141 76.2198 4.47691 80.354 9.99976 80.354H101.53C107.053 80.354 111.53 76.2198 111.53 71.1199V9.25122C111.53 4.15135 107.053 0.0170898 101.53 0.0170898Z"
-                    fill="#4B4B4B"
-                  />
-                </svg>
-              </div>
-
-              <div className="col-span-2">
-                <div className="flex items-center gap-x-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>( 021 ) 666 888 0000</h3>
-                </div>
-                <div className="flex items-center gap-x-2 mt-2">
-                  {" "}
-                  <img src="/green-arrow.png" /> <h3>needhelp@company.com</h3>
-                </div>
-              </div>
-            </div>
+        <div className="flex justify-center mt-4">
+          <div>
+            <p>© 2025 Narrative. All Rights Reserved.</p>
           </div>
         </div>
       </div>
