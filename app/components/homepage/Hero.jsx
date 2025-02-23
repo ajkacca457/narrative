ffcb7da07a438793ead75a5ba51b47f2c61ea8c9
Avijit Karmaker <ajkacca@gmail.com>
2025-02-23 23:35:04 +0200

    add animation to hero section

--- app/components/homepage/Hero.jsx
+++ app/components/homepage/Hero.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import gsap from "gsap";
 import CircularPattern from "../decors/CircularPattern";
 
 const Hero = ({ content }) => {
@@ -7,8 +10,60 @@ const Hero = ({ content }) => {
     hero_subheading = "Default Subheading",
   } = content || {};
 
+  const headingRef = useRef(null);
+  const subheadingRef = useRef(null);
+  const patternRef = useRef(null);
+  const svgPathRef = useRef(null);
+
+  useEffect(() => {
+    const path = svgPathRef.current;
+    const pathLength = path.getTotalLength();
+
+    gsap.set(path, { strokeDasharray: pathLength, strokeDashoffset: pathLength });
+
+    const tl = gsap.timeline();
+    tl.to(path, {
+      strokeDashoffset: 0, 
+      duration: 2.5,
+      ease: "power2.out",
+    });
+
+    // Heading fade-in
+    tl.fromTo(
+      headingRef.current,
+      { opacity: 0, y: 30 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 2,
+        ease: "power2.out",
+      },
+      "-=1.5"
+    );
+
+    tl.fromTo(
+      subheadingRef.current,
+      { opacity: 0, x: -50 },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 1.5,
+        ease: "power2.out",
+      },
+      "-=1" 
+    );
+
+    gsap.to(patternRef.current, {
+      rotation: 360,
+      duration: 8,
+      repeat: -1,
+      ease: "linear",
+    });
+  }, []);
+
   return (
     <div className="w-full h-screen relative flex flex-col items-center justify-center">
+      {/* SVG Decoration with Draw Effect */}
       <svg
         width="194"
         height="127"
@@ -18,6 +73,7 @@ const Hero = ({ content }) => {
         className="absolute top-[200px] left-0"
       >
         <path
+          ref={svgPathRef}
           d="M192.334 1.98498C192.334 1.98498 157.992 7.47165 139.18 18.3194C103.575 38.8496 41.9236 86.7784 78.7164 104.459C110.718 119.837 138.449 88.5693 133.493 62.6156C127.766 32.6285 73.6878 63.485 43.3135 83.0057C15.579 100.83 1.84912 124.936 1.84912 124.936"
           stroke="white"
           strokeOpacity="0.15"
@@ -26,15 +82,24 @@ const Hero = ({ content }) => {
         />
       </svg>
 
-      <CircularPattern customClass="absolute -bottom-[100px] right-0" />
-      <div className=" max-w-[1100px]">
-        <h1 className="text-[96px] text-white font-semibold">
-          {hero_heading
-            ? hero_heading
-            : "We are a digital marketing agency with expertise"}
+      {/* Rotating Circular Pattern */}
+      <div ref={patternRef} className="absolute -bottom-[100px] right-0">
+        <CircularPattern />
+      </div>
+
+      {/* Hero Content */}
+      <div className="max-w-[1100px] text-center">
+        <h1
+          ref={headingRef}
+          className="text-[96px] text-white font-semibold opacity-0"
+        >
+          {hero_heading}
         </h1>
-        <h5 className="text-[20px] ml-[100px] tracking-[.3em] text-white mt-6 uppercase">
-          {hero_subheading ? hero_subheading : "a brand design studio."}
+        <h5
+          ref={subheadingRef}
+          className="text-[20px] ml-[100px] tracking-[.3em] text-white mt-6 uppercase opacity-0"
+        >
+          {hero_subheading}
         </h5>
       </div>
     </div>
