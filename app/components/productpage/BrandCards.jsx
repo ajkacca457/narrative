f028f2dd32139de92ebae9106d19fc955a4edc14
Avijit Karmaker <ajkacca@gmail.com>
2025-03-04 07:46:39 +0200

    add animated border

--- app/components/productpage/BrandCards.jsx
+++ app/components/productpage/BrandCards.jsx
@@ -18,7 +18,7 @@ const BrandCards = ({ content }) => {
     const tl = gsap.timeline({
       scrollTrigger: {
         trigger: sectionRef.current,
-        start: "top 80%", 
+        start: "top 80%",
         toggleActions: "play none none reset",
       },
     });
@@ -33,7 +33,7 @@ const BrandCards = ({ content }) => {
       subtitleRef.current,
       { opacity: 0, y: 20 },
       { opacity: 1, y: 0, duration: 1, ease: "power2.out" },
-      "-=0.5" 
+      "-=0.5"
     );
 
     tl.fromTo(
@@ -44,24 +44,33 @@ const BrandCards = ({ content }) => {
         y: 0,
         duration: 1,
         ease: "power2.out",
-        stagger: 0.2, 
+        stagger: 0.2,
       },
       "-=0.5"
     );
   }, []);
 
   return (
-    <div ref={sectionRef} className="w-full brand-cards-element h-auto py-[20vh]">
+    <div
+      ref={sectionRef}
+      className="w-full brand-cards-element h-auto py-[20vh]"
+    >
       <div className="brand-cards-container w-4/5 lg:w-3/5 mx-auto">
-        <h1 ref={titleRef} className="brand-cards-title text-white text-center text-brand-heading max-w-[17ch] mx-auto opacity-0">
+        <h1
+          ref={titleRef}
+          className="brand-cards-title text-white text-center text-brand-heading max-w-[17ch] mx-auto opacity-0"
+        >
           {title}
         </h1>
 
-        <p ref={subtitleRef} className="text-[#A7A7A7] max-w-[55ch] text-center mx-auto mt-6 opacity-0">
+        <p
+          ref={subtitleRef}
+          className="text-[#A7A7A7] max-w-[55ch] text-center mx-auto mt-6 opacity-0"
+        >
           {subtitle}
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-12">
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-12">
           {cards.length > 0 &&
             cards.map((card, index) => (
               <div
@@ -75,6 +84,30 @@ const BrandCards = ({ content }) => {
                 <p className="text-[#A7A7A7] mt-6 text-[18px] brand-card-description">
                   {card.content}
                 </p>
+              </div>
+            ))}
+        </div> */}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mt-12">
+          {cards.length > 0 &&
+            cards.map((card, index) => (
+              <div class="w-full max-w-[422px] [background:linear-gradient(45deg,#172033,theme(colors.gray.800)_50%,#172033)_padding-box,conic-gradient(from_var(--border-angle),theme(colors.gray.600/.48)_80%,_theme(colors.teal.500)_86%,_theme(colors.teal.300)_90%,_theme(colors.teal.500)_94%,_theme(colors.gray.600/.48))_border-box] rounded-2xl border border-transparent animate-border">
+                <div class="p-5">
+                  <div class="flex justify-between items-center">
+                    <div
+                      key={card.id}
+                      ref={(el) => (cardsRef.current[index] = el)}
+                      className="text-center"
+                    >
+                      <h3 className="text-white text-[22px] font-semibold brand-card-title mt-4">
+                        {card.title}
+                      </h3>
+                      <p className="text-[#A7A7A7] mt-6 text-[18px] brand-card-description">
+                        {card.content}
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,11 +12,11 @@ module.exports = {
         'display-subheading': 'clamp(0.9375rem, -0.1226rem + 4.7115vw, 4rem)', //64px - 15px
         'display-heading': 'clamp(1.5rem, -0.0577rem + 6.9231vw, 6rem)', // 96px - 24px
         'expertise-heading-narrow': 'clamp(1.5rem, 0.9375rem + 2.5vw, 3.125rem)', // 50px - 24px
-        'expertise-heading-highlight':'clamp(2.25rem, 1.4063rem + 3.75vw, 4.6875rem)', // 75px - 36px
+        'expertise-heading-highlight': 'clamp(2.25rem, 1.4063rem + 3.75vw, 4.6875rem)', // 75px - 36px
         'expertise-card-heading': 'clamp(1.5rem, 1.1538rem + 1.5385vw, 2.5rem)', // 40px - 24px 
         'expertise-card-description': 'clamp(1rem, 0.8053rem + 0.8654vw, 1.5625rem)', // 25px - 16px
         'we-do-card-heading': 'clamp(2.25rem, 1.7308rem + 2.3077vw, 3.75rem)', // 60px - 36px
-        'we-do-card-text':'clamp(1.125rem, 0.9736rem + 0.6731vw, 1.5625rem)', // 25px - 18px
+        'we-do-card-text': 'clamp(1.125rem, 0.9736rem + 0.6731vw, 1.5625rem)', // 25px - 18px
         'faq-title': 'clamp(3rem, 1.9615rem + 4.6154vw, 6rem)', // 96px - 48px
         'accordion-heading': 'clamp(0.75rem, 0.4038rem + 1.5385vw, 1.75rem)', // 28px - 12px
         'accordion-content': 'clamp(0.75rem, 0.7067rem + 0.1923vw, 0.875rem)', // 14px - 12px
@@ -28,9 +28,9 @@ module.exports = {
         'square-card-heading': 'clamp(3rem, 2.4159rem + 2.5962vw, 4.6875rem)', // 75px - 48px
         'square-card-title': 'clamp(1.25rem, 1.1635rem + 0.3846vw, 1.5rem)', // 24px - 20px
         'square-card-content': 'clamp(0.875rem, 0.8101rem + 0.2885vw, 1.0625rem)', // 17px - 14px
-        'testimonial-heading':'clamp(2rem, 0.8534rem + 5.0962vw, 5.3125rem)', // 85px - 32px
-        'card-one-heading':'clamp(2.25rem, 1.6442rem + 2.6923vw, 4rem)', // 64px - 36px
-        'card-content':'clamp(0.9375rem, 0.4832rem + 2.0192vw, 2.25rem)', // 36px - 15px
+        'testimonial-heading': 'clamp(2rem, 0.8534rem + 5.0962vw, 5.3125rem)', // 85px - 32px
+        'card-one-heading': 'clamp(2.25rem, 1.6442rem + 2.6923vw, 4rem)', // 64px - 36px
+        'card-content': 'clamp(0.9375rem, 0.4832rem + 2.0192vw, 2.25rem)', // 36px - 15px
         'contact-card': 'clamp(1.25rem, 1.0769rem + 0.7692vw, 1.75rem)', // 28px - 20px
         'form-title': 'clamp(2rem, 1.6538rem + 1.5385vw, 3rem)', // 48px - 32px
         'about-testimonial-heading': 'clamp(2.25rem, 1.7308rem + 2.3077vw, 3.75rem)', // 60px - 36px
@@ -48,6 +48,14 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        'border': 'border 4s linear infinite',
+      },
+      keyframes: {
+        'border': {
+          to: { '--border-angle': '360deg' },
+        }
+      }
     },
   },
   plugins: [],
